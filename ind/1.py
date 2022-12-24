dba710b619e27987c557eb448ea27539ea3e5866
obrMaria <112749225+obrMaria@users.noreply.github.com>
2022-12-24 05:44:21 +0300

    Update 1.py

--- ind/1.py
+++ ind/1.py
@@ -1,25 +1,14 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def br_check(N, a=1):
+def br_check(N, a=1, res=[]):
     if N <= 0 or a == N:
-        return False
+        return res
     elif N != 0:
         b = N - a
-        if b == a:
-            print(b, "+", a, "==", N)
-            return True
-        elif b < a:
-            return True
-        else:
-            print(b, "+", a, "==", N)
-            return br_check(N, a + 1)
-    else:
-        return True
+        res.append((a, b))
+    return br_check(N, a + 1, res)
 
 
 if __name__ == '__main__':
-    if br_check(int(input('Введите N-ое число: '))):
-        print('Вывели все возможные пары чисел')
-    else:
-        print('Нужно ввести натруальные числа, кроме 1!')
+    print((br_check(int(input('Введите N-ое число: ')))))
